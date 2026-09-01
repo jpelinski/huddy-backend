30189ccb31201574c46776bc21516b6448fe3f7e
Jakub Peliński <pelinski.jakub@gmail.com>
2026-09-01 08:43:31 +0200

    feat: add discord bot with event endpoint

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,40 @@ const discord = new Client({
         GatewayIntentBits.DirectMessages,
     ]
 })
-discord.once('ready', () => {
+discord.once('clientReady', () => {
     console.log(`Discord bot logged in as ${discord.user?.tag}`)
 })
 
-discord.login(process.env.DISCORD_TOKEN);
+discord.login(process.env.DISCORD_TOKEN)
+
+app.get('/', (req, res) => {
+    res.json({ status: 'ok' });
+});
+
+app.post('/event', async (req, res) => {
+    const { type, discordId, data } = req.body
+    try {
+        const user = await discord.users.fetch(discordId)
+        let message = ''
+        switch (type) {
+            case 'timer_finished':
+                message = `Your timer |${data.timerName}| has finished!`
+                break;
+            case 'process_network_offline':
+                message = `Netowrk connection of |${data.processName}| is offline!`
+                break;
+            case 'process_offline':
+                message = `Your process |${data.processName}| is offline!`
+                break;
+        }
+        await user.send(message)
+        res.json({ success: true })
+    } catch (error) {
+        console.error('Error fetching user or sending message:', error);
+        res.status(500).json({ success: false, error: 'Failed to send message' });
+    }
+})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
